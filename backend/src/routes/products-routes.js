19bc8cc80e8dec8e92b55e3f00c4cec50e8dd3b0
Ayo Onamusi <onamusia@gmail.com>
2024-08-10 15:27:27 +0100

    MEDI-48 get all product requests by a user

--- backend/src/routes/products-routes.js
+++ backend/src/routes/products-routes.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router(); 
 
 const DUMMY_PRODUCTS = require("../../productsList/productsList.json");
+const DUMMY_PRODUCT_REQUESTS = require("../../productRequestsList/productRequestsLists.json");
 
 
 // GET ALL PRODUCTS
@@ -12,6 +13,14 @@ router.get("/" , (req, res , next) => {
   console.log(`Request received from ${req.headers.host + req.url}` ); 
 });
 
+// GET ALL PRODUCT REQUESTS BY USER ID
+router.get("/user/:userId" , (req, res , next) => {
+  const {userId} = req.params;
+  const userProductRequest = DUMMY_PRODUCT_REQUESTS.filter(p => p.user === userId);
+  console.log("GET Request recevied for a single product by user");
+  res.json({userProductRequest: userProductRequest});
+  console.log(`Request received from  ${req.headers.host + req.url}` );
+});
 
 // GET A PROUCT BY ID/BARCODE
 router.get("/:productId" , (req, res , next) => {
