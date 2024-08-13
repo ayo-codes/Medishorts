ec050e4d8adb6fadb2d10b73a5205ca7384a65bf
Ayo Onamusi <onamusia@gmail.com>
2024-08-13 17:55:33 +0100

    MEDI-61 created user schema, and added unique validation

---
+++ backend/src/models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+
+const productRequest = require("./productRequest");
+
+const {Schema} = mongoose;
+
+
+const userSchema = new Schema({
+  pharmacyPSIRegistrationNo: { type: String, required: true },
+  pharmacyAddress: { type: String, required: true },
+  pharmacyFaxNumber: { type: String, required: true },
+  pharmacyName: { type: String, required: true },
+  pharmacyOwner: { type: String, required: true },
+  pharmacyPhoneNumber: { type: String, required: true },
+  superintendentPharmacist: { type: String, required: true },
+  supervisingPharmacist: { type: String, required: true },
+  vatNumber: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, minlength: 6 },
+  // productRequests: [{ type: mongoose.Types.ObjectId, required: true, ref: "ProductRequest" }],
+  productRequests: [{ type: String, required: true }],
+});
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model("User", userSchema);
