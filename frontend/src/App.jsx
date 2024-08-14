c965f035b3dc5776f917ce3f478c454bb95ce2aa
Ayo Onamusi <onamusia@gmail.com>
2024-08-14 19:15:00 +0100

    added product and product request page test-content and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import { BrowserRouter, Route , Routes , Navigate } from 'react-router-dom'
 import User from './users/pages/users'
+import Products from './products/pages/products'
+import ProductRequests from './productRequests/pages/productRequests'
 
 function App() {
   return <>
     <BrowserRouter>
       <Routes>
         <Route path="/user" element={<User/>} />
+        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/product" element={<Products/>} />
+        <Route path="/product-request" element={<ProductRequests/>} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
