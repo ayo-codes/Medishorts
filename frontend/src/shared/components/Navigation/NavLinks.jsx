f99a1d9712e3277d3e9a03e66a9b307f700406a5
Ayo Onamusi <onamusia@gmail.com>
2024-08-21 09:57:02 +0100

    MEDI-83 Added Conditional Display of Sign in button on NavLinks component

--- frontend/src/shared/components/Navigation/NavLinks.jsx
+++ frontend/src/shared/components/Navigation/NavLinks.jsx
@@ -1,39 +1,43 @@
-import {React, useContext} from "react"
-import PropTypes from "prop-types"
-import {NavLink} from "react-router-dom"
-import { AuthContext } from "../../context/AuthContext"
+import { React, useContext } from "react";
+import PropTypes from "prop-types";
+import { NavLink } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 
-const NavLinks = props => {
+const NavLinks = (props) => {
   const auth = useContext(AuthContext);
   return (
     <ul>
-        <li>
-          <NavLink to="/" exact >Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/product-requests">Product Requests</NavLink>
-        </li>
-        <li>
-          <NavLink to="/product-requests/new">Add Product Request</NavLink>
-        </li>
-        <li>
-          <NavLink to="/products">Products</NavLink>
-        </li>
-        <li>
-          <NavLink to="/product/new">Add Product</NavLink>
-        </li>
+      <li>
+        <NavLink to="/" exact>
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/product-requests">Product Requests</NavLink>
+      </li>
+      <li>
+        <NavLink to="/product-requests/new">Add Product Request</NavLink>
+      </li>
+      <li>
+        <NavLink to="/products">Products</NavLink>
+      </li>
+      <li>
+        <NavLink to="/product/new">Add Product</NavLink>
+      </li>
+      {!auth.isLoggedIn && (
         <li>
           <NavLink to="/auth">Sign In</NavLink>
-          {auth.isLoggedIn && 
-            <li>
-          <button onClick={auth.logout}>Logout</button>
-          </li>}
         </li>
+      )}
+      {auth.isLoggedIn && (
+        <li>
+          <button onClick={auth.logout}>Logout</button>
+        </li>
+      )}
     </ul>
-    
-  )
-}
+  );
+};
 
-NavLinks.propTypes = {}
+NavLinks.propTypes = {};
 
-export default NavLinks
+export default NavLinks;
