ae9651ecaf65437d2feb07c498d65751b8a5c162
Ayo Onamusi <onamusia@gmail.com>
2024-08-22 18:54:33 +0100

    MEDI-96 created a new checkAuth middleware to protect routes on the server and implemented it in the product-requests-routes.js

---
+++ backend/src/middleware/check-auth.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+
+const ApiHttpError = require("../models/api-http-error");
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];// split the token from the Bearer on the white space
+    if (!token) {
+      throw new Error("Authentication failed");
+    }
+    const decodedToken = jwt.verify(token, process.env.JWT_KEY);
+    req.userData = { userId: decodedToken.userId };
+    next();
+  } catch (err) {
+    const error = new ApiHttpError("Authentication failed", 401);
+    return next(error);
+  }
+}
--- backend/src/routes/product-requests-routes.js
+++ backend/src/routes/product-requests-routes.js
@@ -1,14 +1,22 @@
 const { Router } = require("express");
 
 const { check } = require("express-validator");
+const checkAuth = require("../middleware/check-auth");
 
 const productRequestsControllers = require("../controllers/product-requests-controller");
 
-const router = Router();
 
+const router = Router();
 
 // GET ALL PRODUCT REQUESTS
 router.get("/", productRequestsControllers.getAllProductRequests);
+
+// GET A PRODUCT REQUEST BY ID
+router.get("/:productRequestId", productRequestsControllers.getProductRequestById);
+
+// ROUTE PROTECTION MIDDLEWARE
+router.use(checkAuth);
+
 
 // CREATE A NEW PRODUCT REQUEST
 // router.post("/", [check("productName").notEmpty(), check("genericName").notEmpty(), check("packSize").notEmpty()], productRequestsControllers.createProductRequest);
@@ -17,8 +25,6 @@ router.post("/", [check("productName").notEmpty(), check("genericName").notEmpty
 // GET ALL PRODUCT REQUESTS BY USER ID
 router.get("/user/:userId", productRequestsControllers.getProductRequestsByUserId);
 
-// GET A PRODUCT REQUEST BY ID
-router.get("/:productRequestId", productRequestsControllers.getProductRequestById);
 
 // UPDATE A PRODUCT REQUEST BY ID
 router.patch("/:productRequestId", [check("productName").notEmpty(), check("genericName").notEmpty(),], productRequestsControllers.updateProductRequestById);
