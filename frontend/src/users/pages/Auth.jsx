d86497779912787fb599179339baec87ac86b8e3
Ayo Onamusi <onamusia@gmail.com>
2024-08-24 23:48:44 +0100

    MEDI-105 moved login-signup switcher closer to new login form

--- frontend/src/users/pages/Auth.jsx
+++ frontend/src/users/pages/Auth.jsx
@@ -1,6 +1,6 @@
 import { useState , useContext } from "react";
 
-import { Button } from "@mui/material";
+import { Button , Box , Stack } from "@mui/material";
 
 import LoginForm from "../components/LoginForm";
 import SignUpForm from "../components/SignUpForm";
@@ -16,11 +16,14 @@ const Auth = () => {
     console.log("Switching Mode");
   }
   return (
-    <>
+    <Stack>
       {isLoginMode && <LoginForm /> } 
       {!isLoginMode && <SignUpForm />}
-      <Button color="primary" variant="outlined"  onClick={handleSwitchMode}> {isLoginMode ? "Don't Have an Account? SIGNUP" : "Already Have an Account? LOGIN"} </Button>
-    </>
+
+      <Box display="flex" justifyContent="center" margin={-8}>
+        <Button color="primary" variant="outlined"  onClick={handleSwitchMode}> {isLoginMode ? "Don't Have an Account? SIGNUP" : "Already Have an Account? LOGIN"} </Button>
+      </Box>
+    </Stack>
   );
 };
 
