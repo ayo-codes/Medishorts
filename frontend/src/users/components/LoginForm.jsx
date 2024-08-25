67da892f9ec5d49a357b7518819d56404e8a6cb2
Ayo Onamusi <onamusia@gmail.com>
2024-08-25 01:18:35 +0100

    MEDI-105 Updated Signup Form UI with MUI

--- frontend/src/users/components/LoginForm.jsx
+++ frontend/src/users/components/LoginForm.jsx
@@ -2,7 +2,15 @@ import { useEffect, useContext, useState } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 
-import { TextField, Box , Stack , Typography , Paper, CircularProgress , Button } from "@mui/material";
+import {
+  TextField,
+  Box,
+  Stack,
+  Typography,
+  Paper,
+  CircularProgress,
+  Button,
+} from "@mui/material";
 
 import { DevTool } from "@hookform/devtools";
 
@@ -11,7 +19,7 @@ import { medishortsService } from "../../services/medishorts-service";
 
 const LoginForm = () => {
   // Gain access to object properties from the AuthContextProvider
-  const  auth  = useContext(AuthContext);
+  const auth = useContext(AuthContext);
 
   // Set States for loading and error
   const [isLoading, setIsLoading] = useState(false);
@@ -78,82 +86,89 @@ const LoginForm = () => {
   }, [isSubmitSuccessful, reset]);
 
   return (
-    <Box component ="div" display="flex" justifyContent="center" alignItems="center" height="50vh"  >
+    <Box
+      component="div"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      height="50vh"
+    >
       <Paper elevation={3} sx={{ padding: 4, maxWidth: 400, width: "100%" }}>
-
-        <Typography align="center" variant="h5" gutterBottom>Login To Your Account</Typography>
+        <Typography align="center" variant="h5" gutterBottom>
+          Login To Your Account
+        </Typography>
         {/* Form Submission logic and using the handleSubmit method from useForm */}
         <form onSubmit={handleSubmit(onSubmitAuthRequest, onError)} noValidate>
-            <Box mb={2}>
-              <TextField
+          <Box mb={2}>
+            <TextField
               fullWidth
-                type="email"
-                id="email"
-                label="Email"
-                size="small"
-                variant="outlined"
-                placeholder="Your Email"
-                error={errors.email ? true : false}
-                helperText={errors.email ? errors.email.message : null}
+              type="email"
+              id="email"
+              label="Email"
+              size="small"
+              variant="outlined"
+              placeholder="Your Email"
+              error={errors.email ? true : false}
+              helperText={errors.email ? errors.email.message : null}
+              {...register("email", {
+                pattern: {
+                  value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                  message: "Invalid Email",
+                },
+                required: { value: true, message: "Your Email is required" },
+                minLength: { value: 5, message: "Min length is 5" },
+              })}
+            />
+          </Box>
+          <Box mb={2}>
+            <TextField
+              fullWidth
+              type="password"
+              id="password"
+              label="Password"
+              size="small"
+              variant="outlined"
+              placeholder="Password"
+              error={errors.password ? true : false}
+              helperText={errors.password ? errors.password.message : null}
+              {...register("password", {
+                required: { value: true, message: "Password is required" },
+                minLength: { value: 5, message: " Min length is 5" },
+              })}
+            />
+          </Box>
 
-                {...register("email", {
-                  pattern: {
-                    value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-                    message: "Invalid Email",
-                  },
-                  required: { value: true, message: "Your Email is required" },
-                  minLength: { value: 5, message: "Min length is 5" },
-                })}
-                
-              />
-            </Box>
-            <Box mb={2}>
-              <TextField
-                fullWidth
-                type="password"
-                id="password"
-                label="Password"
-                size="small"
-                variant="outlined"
-                placeholder="Password"
-                error={errors.password ? true : false}
-                helperText={errors.password ? errors.password.message : null}
+          {/* Manage Loading Icon */}
+          <Box justifyContent="center" sx={{ display: "flex" }}>
+            {isLoading && <CircularProgress />}
+          </Box>
 
-                {...register("password", {
-                  required: { value: true, message: "Password is required" },
-                  minLength: { value: 5, message: " Min length is 5" },
-                })}                
-              />
-               {/* Manage Loading Icon */}
-              <Box justifyContent="center" sx={{display:"flex"}}>
-                {isLoading && <CircularProgress />}
-              </Box> 
+          {/* Manage Error Messages */}
+          <Box>{error && <Typography variant="h6">{error}</Typography>}</Box>
 
-              {/* Manage Error Messages */}
-              {error && <Typography variant="h6">{error}</Typography>}
-              {/* Manage the button state based on user actions */}
-            </Box>
+          {/* Manage the button state based on user actions */}
 
-            <Box mb={2}>    
-              <Button 
-                type="submit"
-                variant="contained"
-                color="primary"
-                fullWidth
-                disabled={isSubmitting}>
-                Login
-              </Button>
+          <Box mb={2}>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              fullWidth
+              disabled={isSubmitting}
+            >
+              Login
+            </Button>
 
-              <Box mt={1} textAlign="center">
+            <Box mt={1} textAlign="center">
               <Button type="button" variant="text" onClick={() => reset()}>
                 Reset
               </Button>
-              </Box>
-            </Box>  
-          </form>
-          {/* To manage the devtool visuals */}
-          <DevTool control={control} />
-      </Paper>  
+            </Box>
+          </Box>
+        </form>
+        {/* To manage the devtool visuals */}
+        <DevTool control={control} />
+      </Paper>
     </Box>
   );
 };
--- frontend/src/users/pages/Auth.jsx
+++ frontend/src/users/pages/Auth.jsx
@@ -1,27 +1,29 @@
-import { useState , useContext } from "react";
+import { useState, useContext } from "react";
 
-import { Button , Box , Stack } from "@mui/material";
+import { Button, Box, Stack } from "@mui/material";
 
 import LoginForm from "../components/LoginForm";
 import SignUpForm from "../components/SignUpForm";
 
-
 const Auth = () => {
-
-
   const [isLoginMode, setIsLoginMode] = useState(true);
 
   const handleSwitchMode = () => {
     setIsLoginMode((previousMode) => !previousMode);
     console.log("Switching Mode");
-  }
+  };
   return (
     <Stack>
-      {isLoginMode && <LoginForm /> } 
+      {isLoginMode && <LoginForm />}
       {!isLoginMode && <SignUpForm />}
 
-      <Box display="flex" justifyContent="center" margin={-8}>
-        <Button color="primary" variant="outlined"  onClick={handleSwitchMode}> {isLoginMode ? "Don't Have an Account? SIGNUP" : "Already Have an Account? LOGIN"} </Button>
+      <Box display="flex" justifyContent="center" margin={-2}>
+        <Button color="primary" variant="outlined" onClick={handleSwitchMode}>
+          {" "}
+          {isLoginMode
+            ? "Don't Have an Account? SIGNUP"
+            : "Already Have an Account? LOGIN"}{" "}
+        </Button>
       </Box>
     </Stack>
   );
