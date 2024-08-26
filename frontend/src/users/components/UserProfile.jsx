03ffc4c995a2936c9f37ade21ffa14b844a011f4
Ayo Onamusi <onamusia@gmail.com>
2024-08-26 12:18:16 +0100

    MEDI-110 created <UserProfile> Component and included it in the <User> page

---
+++ frontend/src/users/components/UserProfile.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Box , Typography } from "@mui/material";
+
+const UserProfile = () => {
+  return (
+    <Box>
+      <Typography variant="h3" align="center" m={2}>
+      UserProfile
+      </Typography>
+    </Box>
+  )
+};
+
+export default UserProfile;
--- frontend/src/users/pages/User.jsx
+++ frontend/src/users/pages/User.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import UserProfile from "../components/UserProfile";
+
+
+import {Box , Typography} from "@mui/material";
+
 
 const User = () => {
   return (
-    <div>
-      <h1>Users Page</h1>
-    </div>
+    <Box>
+      <Typography variant="h3" align="center" m={2}>Users Page</Typography>
+      <UserProfile />
+    </Box>
   );
 }
 
