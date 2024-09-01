a21b3508a6aa4a7e4e35b95e11d0a0872eff1d25
Ayo Onamusi <onamusia@gmail.com>
2024-09-01 19:54:28 +0100

    MEDI-122 created a new page to display the list of shorts from HPRA scrapped data

---
+++ frontend/src/scrappedShortProductsHpra/pages/ShortProductsHpraTable.jsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import { medishortsService } from "../../services/medishorts-service";
+import ShortProductsHpraListTable from "../components/ShortProductsHpraListTable";
+import DummyProductRequestData from "../../../../backend/src/dummy_data/productRequestsList/productRequestsLists.json";
+import { Box , Typography } from "@mui/material";
+
+const ShortProductsHpraTable = () => {
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [loadedShortProductsHpra, setLoadedShortProductsHpra] = useState([]);
+
+  
+  useEffect(() => {
+    const sendRequest = async () => {
+      setIsLoading(true);
+      const response = await medishortsService.getAllShortProductsHpra();
+      console.log(response.shortProductsHpra);
+      
+      if (response.state !== true) {
+        setIsLoading(false);
+        setError(response.error);
+        console.log(response.error);
+      }
+
+      if(response.state === true){
+        // Filter the product requests to only include those with shortProduct === true
+  
+        setLoadedShortProductsHpra(response.shortProductsHpra);
+        console.log(response.shortProductsHpra);
+        setIsLoading(false);
+        setError(null);
+      }
+    };
+
+    sendRequest();
+  }, []);
+
+//   const productRequestDeleteHandler = (deletedProductRequestId) => {
+//     console.log("Deleting product request...");
+//     setLoadedShortProductsHpra((prevProductRequests) => {
+//       return prevProductRequests.filter((productRequest) => productRequest.id !== deletedProductRequestId); 
+//   }
+// )}
+
+  // const DUMMY_PRODUCT_REQUESTS = DummyProductRequestData.slice(0, 10);
+
+
+  return (
+    <Box>
+      {/* <Typography>Product Requests</Typography> */}
+      {isLoading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      {!isLoading && loadedShortProductsHpra && <ShortProductsHpraListTable items={loadedShortProductsHpra} onDeleteProductRequest={productRequestDeleteHandler} />}
+    </Box>
+  );
+}
+
+export default ShortProductsHpraTable;
