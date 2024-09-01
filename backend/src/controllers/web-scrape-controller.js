4a248db08fe6add29270db141f04c8bbf3cb8092
Ayo Onamusi <onamusia@gmail.com>
2024-09-01 19:05:49 +0100

    MEDI-120 updated method to delete from shortProductsHpra before writing to database

--- backend/src/controllers/web-scrape-controller.js
+++ backend/src/controllers/web-scrape-controller.js
@@ -9,11 +9,22 @@ const webScrapper = require("../web-scrapper/webScrapper");
 
 
 // WEB SCRAPPER CONTROLLER TO SAVE the shortProductsHpra JSON data from webScrapper.js to MongoDB
+// eslint-disable-next-line consistent-return
 const webScrapeTrigger = async (req, res, next) => {
   console.log ("Web Scrape Request received");
   const scrappedData = await webScrapper.webScrapper();
-  res.send("Web Scrape Request received");
-  console.log(`scrappedData: ${scrappedData[0].productName}`);
+  
+  try {
+    // Save the scrappedData to MongoDB using mongoose
+    await ShortProductsHpra.deleteMany({});
+    await ShortProductsHpra.insertMany(scrappedData);
+    console.log("Data saved to MongoDB");
+  } catch (error) {
+    const apiError = new ApiHttpError("Failed to save data to MongoDB", 500);
+    return next(apiError);
+  }  
+
+  return res.json({ scrappedData });
 };
 
 
